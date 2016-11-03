b355ccaf4d21b598c8b66a56c63d0763dfe91cea
Peter-Jan <pjan89@gmail.com>
2016-11-03 03:41:28 -0400

    Add files via upload

---
+++ Block.cpp
@@ -0,0 +1,52 @@
+class Block
+{
+public:
+	Block();
+	int getX() const;
+	int getY() const;
+	int getZ() const;
+	void setCoordinate(const int xin, const int yin, const int zin);
+	void setDimension(const int win, const int hin, const int lin);
+	void setColor(const int cin);
+
+private:
+	int x, y, z;
+	int color;
+	int w, h, l;
+
+};
+
+Block::Block()
+{
+	setDimension(10, 10, 10);
+}
+void Block::setDimension(const int win, const int hin, const int lin)
+{
+	w = win;
+	h = hin;
+	l = lin;
+}
+int Block::getX() const
+{
+	return x;
+}
+int Block::getY() const
+{
+	return y;
+}
+int Block::getZ() const
+{
+	return z;
+}
+void Block::setCoordinate(const int xin, const int yin,const int zin)
+{
+	x = xin;
+	y = yin;
+	z = zin;
+}
+void Block::setColor(const int cin)
+{
+	color = cin;
+}
+
+}
