7d4cbec2310f4d1de780489500edf7431d7da6c4
Rockarel <cameron.stabile@gmail.com>
2016-11-16 20:03:08 -0500

    Update Block.h

--- Block.h
+++ Block.h
@@ -1,16 +1,28 @@
+#ifndef BLOCK_CLASS_INCLUDED
+#define BLOCK_CLASS_INCLUDED
+
+#include <memory>
 #include <vector>
-#ifndef BLOCK_H
-#define BLOCK_H
+#include <stdlib.h>
+#include "fssimplewindow.h"
+#include <math.h>
 
-
-const int roomSize = 100;
-const int blockCount = roomSize * roomSize;
+#ifndef TRUE
+#define TRUE 1
+#endif // !TRUE
 
 class Block
 {
 public:
+	int roomSize = 10;
 	Block();
-	Block::Block(int x, int y, int z);
+	Block(int roomSize);
+	Block(int roomSize, int x, int y, int z);
+	Block(const std::unique_ptr<Block> &fromPtr);
+	Block &operator=(const std::unique_ptr<Block> &fromPtr);
+	void Initialize(int roomSize, int x, int y, int z);
+	void copyFrom(const Block &fromBlock);
+
 	~Block();
 	int getX() const;
 	int getY() const;
@@ -27,10 +39,11 @@ private:
 	int r, g, b;
 	int w, h, l;
 public:
-	double blockSize = 8;
+	double blockSize = 8.0;
 	int index = 0;
 	int sideVisible[6] = { 1,1,1,1,1,1 };
+	int renderable = TRUE;
 	std::vector<double> pos = { 0,0,0 };
 };
 
-#endif
+#endif // !BLOCK_CLASS_INCLUDED
