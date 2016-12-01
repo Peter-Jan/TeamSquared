b2a5f398f2d94fa1c4ac016aeb3ebef08edf8027
Rockarel <cameron.stabile@gmail.com>
2016-11-30 19:41:43 -0500

    a

--- CameraObject.h
+++ CameraObject.h
@@ -16,7 +16,7 @@ public:
 	int roomSize=100;
 	int index = 0;
 	double h, p, b;
-	double dxMove, dyMove, dzMove;
+	double dx, dy, dz;
 	double vertVel, camHeight, zoom, viewRadius, sensitivity, fov, nearZ, farZ, coneAngle;
 	std::vector<double> forwardVector = { 0,0,0 };
 	std::vector<double> pos = { 0,0,0 };
