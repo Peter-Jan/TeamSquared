d4c9c11da0dfae2c6b629ac9c6c130cd82eaacce
Loren Russell <lorenkrussell@gmail.com>
2016-12-01 19:54:13 -0500

    added materialBlock files back, whoops

---
+++ materialBlock.h
@@ -0,0 +1,33 @@
+#ifndef MATERIAL_BLOCK
+#define MATERIAL_BLOCK
+
+#include "Block.h"
+#include <string>
+
+class materialBlock: public Block
+{
+public:
+    materialBlock();
+    materialBlock(int roomsize, int x, int y, int z, std::vector<int> matVec);
+    ~materialBlock();
+    void initialize();
+    void setImage(int imageX,int imageY);
+    void setColor(int matR, int matG, int matB);
+    void setParam(int health, int quantity);
+    void setName(std::string inStr);
+    void setXYZ(double x, double y, double z);
+    int getImx() const;
+    int getImy() const;
+    std::string getName() const;
+protected:
+    int imageX, imageY;
+    int healthParam, quantParam,health;
+    std::string name;
+};
+
+
+
+
+
+#endif // !MATERIAL_BLOCK
+
